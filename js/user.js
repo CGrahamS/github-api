7f9cac380ee1d7b22a293b5a4c2245c0f2033f3f
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-10-14 05:01:32 -0700

    add profile picture api request

--- js/user.js
+++ js/user.js
@@ -4,6 +4,7 @@ exports.getRepos = function(username){
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
     console.log(JSON.stringify(response));
     $('.username').text(response.login);
+    $('#profile-pic').append('<img src="'+ response.avatar_url + '" alt="' + response.name + ', profile picture" />')
     $('#name').text(response.name);
     $.get(response.repos_url).then(function(repositories){
       for(repository of repositories) {
@@ -11,10 +12,10 @@ exports.getRepos = function(username){
       }
       console.log(response);
     }).fail(function(error){
-      console.log(error.responseJSON.message);
+      $('#repositories').append('<li>' + error.responseJSON.message + '</li>');
     });
   }).fail(function(error){
-    console.log(error.responseJSON.message);
+    $('.username').text(error.responseJSON.message);
   });
 };
 
