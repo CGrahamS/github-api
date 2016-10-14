04f895a47e6abe0424522850c16dd43e2524245b
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-10-14 04:50:31 -0700

    display username, name and repositories via api request

--- js/user.js
+++ js/user.js
@@ -2,7 +2,17 @@ var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(username){
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-    console.log(response);
+    console.log(JSON.stringify(response));
+    $('.username').text(response.login);
+    $('#name').text(response.name);
+    $.get(response.repos_url).then(function(repositories){
+      for(repository of repositories) {
+        $('#repositories').append('<li>' + repository.name + ', ' + repository.language + '</li>');
+      }
+      console.log(response);
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
